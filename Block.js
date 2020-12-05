1e6274b06ce818e9104882846b6a40b0cc68e92d
TahaM8749 <71301283+TahaM8749@users.noreply.github.com>
2020-12-04 18:13:58 -0800

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,42 @@
+class Block {
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':0.009,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visiblity = 255;
+        World.add(world, this.body);
+      }
+      display() {
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        console.log(this.body.speed)
+        if(this.body.speed < 2) {
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0,0,this.width, this.height);
+          pop();
+         }
+         else {
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           //rect(this.body.position.x, this.body.position.y, 30, 40);
+           pop();
+
+          }
+      }
+      score() {
+        if(this.Visibility<0 && this.Visibility>-105) {
+          score++;
+        }
+      }
+        
+}
